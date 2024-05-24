b16bc098e6ba89d6318a8e384e5bee70b42166a8
Elena Pakulina <el.pakulina@yandex.ru>
2024-05-24 15:20:17 +0500

    мелкофикс _ рефакторинг

--- src/main/resources/db/scripts/004_ddl_create_priorities_table.sql
+++ src/main/resources/db/scripts/004_ddl_create_priorities_table.sql
@@ -4,10 +4,3 @@ CREATE TABLE priorities (
    position int,
    description VARCHAR(128) NOT NULL
 );
-
-INSERT INTO priorities (name, position, description) VALUES ('urgently', 1, 'Срочный');
-INSERT INTO priorities (name, position, description) VALUES ('normal', 2, 'Нормальный');
-
-ALTER TABLE tasks ADD COLUMN priority_id int REFERENCES priorities(id);
-
-UPDATE tasks SET priority_id = (SELECT id FROM priorities WHERE name = 'urgently');
---
+++ src/main/resources/db/scripts/005_dml_insert_into_priorities.sql
@@ -0,0 +1,2 @@
+INSERT INTO priorities (name, position, description) VALUES ('urgently', 1, 'Срочный');
+INSERT INTO priorities (name, position, description) VALUES ('normal', 2, 'Нормальный');
